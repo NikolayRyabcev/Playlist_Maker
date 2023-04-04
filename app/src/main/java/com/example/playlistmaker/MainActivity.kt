776b2b0e9911b1a5c82283a9819c45aceb22f549
NikolayRyabcev <nikolas14@mail.ru>
2023-04-04 20:41:12 +0300

    Плэйлист загрузка 5

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val butt1 = findViewById<Button>(R.id.button1)
+        val butt3 = findViewById<Button>(R.id.button3)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val butt2 = findViewById<Button>(R.id.button2)
+
+        butt2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+        }
+        butt1.setOnClickListener(imageClickListener)
+        butt2.setOnClickListener(imageClickListener)
+        butt3.setOnClickListener(imageClickListener)
     }
 }
