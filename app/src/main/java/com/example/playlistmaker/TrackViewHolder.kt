0dc6d54e0c683554706cb6b7c3a0e7dc8c17a3c2
NikolayRyabcev <nikolas14@mail.ru>
2023-05-01 21:42:05 +0300

    Спринт 10 Загрузка 3

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,4 +1,25 @@
 package com.example.playlistmaker
 
-class TrackViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val trackImage : ImageView = itemView.findViewById(R.id.trackImage)
+    val trackName : TextView = itemView.findViewById(R.id.trackName)
+    val groupName : TextView = itemView.findViewById(R.id.groupName)
+    val trackTime : TextView = itemView.findViewById(R.id.trackTime)
+
+    fun bind (item:Track) {
+        val adaptedTrackName = item.trackName
+        val adaptedArtistName = item.artistName
+        val adaptedTrackTime = item.trackTime
+        val adaptedTrackPicture = item.artworkUrl100
+
+        //trackImage.
+        trackName.text=adaptedTrackName
+        groupName.text=adaptedArtistName
+        trackTime.text=adaptedTrackTime
+    }
 }
