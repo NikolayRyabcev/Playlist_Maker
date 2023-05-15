af44ad6d029e48b79323935a9927c63cc35d05ab
NikolayRyabcev <nikolas14@mail.ru>
2023-05-15 21:59:50 +0300

    Спринт 10 Загрузка 9

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -9,25 +9,25 @@ import com.bumptech.glide.Glide
 
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val trackImage : ImageView = itemView.findViewById(R.id.trackImage)
-    val trackName : TextView = itemView.findViewById(R.id.trackName)
-    val groupName : TextView = itemView.findViewById(R.id.groupName)
-    val trackTime : TextView = itemView.findViewById(R.id.trackTime)
+    val trackImage: ImageView = itemView.findViewById(R.id.trackImage)
+    val trackName: TextView = itemView.findViewById(R.id.trackName)
+    val groupName: TextView = itemView.findViewById(R.id.groupName)
+    val trackTime: TextView = itemView.findViewById(R.id.trackTime)
 
-    fun bind (item:Track) {
+    fun bind(item: Track) {
         val adaptedTrackName = item.trackName
         val adaptedArtistName = item.artistName
         val adaptedTrackTime = item.trackTime
 
-            Glide.with(itemView)
-                .load(item.artworkUrl100)
-                .placeholder(R.drawable.musicalbum)
-                .into(trackImage)
+        Glide.with(itemView)
+            .load(item.artworkUrl100)
+            .placeholder(R.drawable.musicalbum)
+            .into(trackImage)
 
 
 
-        trackName.text=adaptedTrackName
-        groupName.text=adaptedArtistName
-        trackTime.text=adaptedTrackTime
+        trackName.text = adaptedTrackName
+        groupName.text = adaptedArtistName
+        trackTime.text = adaptedTrackTime
     }
 }
