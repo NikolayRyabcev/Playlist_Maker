f1946df46e64ab6ba3632d42040bede0cd85ccb6
NikolayRyabcev <nikolas14@mail.ru>
2023-05-17 22:00:54 +0300

    Спринт 11 загрузка 1

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -6,13 +6,26 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+    private val iTunesBaseURL = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseURL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(iTunesSearchAPI::class.java)
 
     val KEY_TEXT = ""
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,43 +64,50 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val trackList: List<Track> = listOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
+        val trackList = ArrayList<Track>()
         val recyclerView = findViewById<RecyclerView>(R.id.trackRecycler)
         recyclerView.layoutManager = LinearLayoutManager(this)
-
         val trackAdapter = TrackAdapter(trackList)
         recyclerView.adapter = trackAdapter
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+                if (inputEditText.text.isNotEmpty()) {
+                    iTunesService.search(inputEditText.text.toString())
+                        .enqueue(object : Callback<TrackResponse> {
+
+                            override fun onResponse(
+                                call: Call<TrackResponse>,
+                                response: Response<TrackResponse>
+                            ) {
+                                if (response.code() == 200) {
+                                    trackList.clear()
+                                    if (response.body()?.tracksResponse?.isNotEmpty() == true) {
+                                        trackList.addAll(response.body()!!.tracksResponse)
+                                        trackAdapter.notifyDataSetChanged()
+                                    }
+                                    if (trackAdapter.tracks.isEmpty()) {
+
+                                    }
+                                }
+                            }
+
+                            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                                TODO("Not yet implemented")
+                            }
+
+
+                        })
+                }
+
+
+                true
+            }
+            false
+        }
+
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
