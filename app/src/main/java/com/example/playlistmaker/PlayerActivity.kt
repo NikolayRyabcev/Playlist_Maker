687a81769fb3df835a3a486659fb0269bed4cb13
NikolayRyabcev <nikolas14@mail.ru>
2023-06-16 15:37:49 +0300

    Версия 13.3 цвета + кнопка назад

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,11 +1,16 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player_activity)
+        val arrowButton = findViewById<ImageView>(R.id.playerBackButtonArrow)
+        arrowButton.setOnClickListener {
+            finish()
+        }
     }
 }
