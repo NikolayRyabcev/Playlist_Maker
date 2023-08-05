c4bafaa503d313da81a646991057ce2aac711bcb
NikolayRyabcev <nikolas14@mail.ru>
2023-08-06 01:00:04 +0300

    Версия 16.9.1 гл экран готов, а темная тема - нет

--- app/src/main/java/com/example/playlistmaker/UI/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/UI/main/activity/MainActivity.kt
@@ -1,37 +1,28 @@
 package com.example.playlistmaker.UI.main.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.playlistmaker.R
-import com.example.playlistmaker.UI.main.view_model.MainViewModel
 import com.example.playlistmaker.UI.media_library.Activities.MediaLibraryActivity
 import com.example.playlistmaker.UI.search.activity.SearchActivity
 import com.example.playlistmaker.UI.settings.activity.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainViewModel: MainViewModel
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //делаем viewmodel
-        mainViewModel = ViewModelProvider(
-            this,
-            MainViewModel.getViewModelFactory()
-        )[MainViewModel::class.java]
-
-        //вызываем кнопки
-        val searchButton = findViewById<Button>(R.id.SearchButton)
+        val srchButton = findViewById<Button>(R.id.SearchButton)
         val mediaLibButton = findViewById<Button>(R.id.MediaLibButtom)
         val settButton = findViewById<Button>(R.id.SettingsButton)
 
-        //ставим обработку кликов
-        searchButton.setOnClickListener { mainViewModel.pressSearch() }
-        mediaLibButton.setOnClickListener { mainViewModel.pressMediaTech() }
-        settButton.setOnClickListener { mainViewModel.pressSettings() }
+        srchButton.setOnClickListener { startActivity(Intent(this, SearchActivity::class.java)) }
+        mediaLibButton.setOnClickListener { startActivity(Intent(this, MediaLibraryActivity::class.java)) }
+        settButton.setOnClickListener { startActivity(Intent(this, SettingsActivity::class.java)) }
     }
 }
