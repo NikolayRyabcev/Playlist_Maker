37078283ffd395770cc2d210beab1baa6084cf0c
Nikolay <nikolas14@mail.ru>
2023-08-30 17:02:49 +0300

    Спринт 18.1.1

---
+++ app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/fragments/favouritesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker.ui.mediaLibrary.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.databinding.FragmentFavouritesBinding
+
+class favouritesFragment : Fragment(){
+    private var nullableFavouritesBinding : FragmentFavouritesBinding? = null
+    private val favouritesBinding = nullableFavouritesBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        nullableFavouritesBinding=FragmentFavouritesBinding.inflate(inflater, container,false)
+    return favouritesBinding.root
+    }
+}
