7441a66beda47c0a06547773cfeedc37065e1671
Nikolay <nikolas14@mail.ru>
2023-09-01 17:35:44 +0300

    Спринт 18.1.4

--- app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/Activities/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/Activities/MediaLibraryActivity.kt
@@ -2,17 +2,22 @@ package com.example.playlistmaker.ui.mediaLibrary.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.PagerAdapter
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMediaLibraryBinding
 import com.example.playlistmaker.ui.mediaLibrary.fragments.FavouritesFragment
+import com.example.playlistmaker.ui.mediaLibrary.fragments.FragmentAdapter
+import com.example.playlistmaker.ui.mediaLibrary.fragments.SelectPage
+import com.google.android.material.tabs.TabLayoutMediator
 
-class MediaLibraryActivity : AppCompatActivity() {
+class MediaLibraryActivity : AppCompatActivity(), SelectPage {
     private lateinit var binding: ActivityMediaLibraryBinding
+    private lateinit var tabMediator: TabLayoutMediator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMediaLibraryBinding.inflate(layoutInflater)
+        binding = ActivityMediaLibraryBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if (savedInstanceState==null) {
+        if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.favouriteTracks, FavouritesFragment())
                 .commit()
@@ -20,5 +25,16 @@ class MediaLibraryActivity : AppCompatActivity() {
         binding.backButtonArrow.setOnClickListener {
             finish()
         }
+        val adapter = FragmentAdapter(supportFragmentManager, lifecycle)
+        binding.pager.adapter = adapter
+        tabMediator.attach()
+    }
+
+    override fun NavigateTo(page: Int) {
+        binding.pager.currentItem = page
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
     }
 }
--- app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/fragments/FragmentAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/fragments/FragmentAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.playlistmaker.ui.mediaLibrary.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class FragmentAdapter (hostFragment: Fragment) : FragmentStateAdapter(hostFragment) {
-    override fun getItemCount(): Int =2
+class FragmentAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
-        return if (position ==0) FavouritesFragment() else PlaylistFragment()
+        return if (position == 0) FavouritesFragment() else PlaylistFragment()
     }
 }
