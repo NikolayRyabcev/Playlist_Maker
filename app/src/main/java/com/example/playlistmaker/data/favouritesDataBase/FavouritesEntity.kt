5f99d7b5733613f04a4c2189b8ff6f4da034c0fe
Nikolay <nikolas14@mail.ru>
2023-10-11 21:13:00 +0300

    Спринт 21.5.1

--- app/src/main/java/com/example/playlistmaker/data/favouritesDataBase/FavouritesEntity.kt
+++ app/src/main/java/com/example/playlistmaker/data/favouritesDataBase/FavouritesEntity.kt
@@ -3,16 +3,18 @@ package com.example.playlistmaker.data.favouritesDataBase
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.sql.Types.INTEGER
 
-@Entity(tableName="favourites_table")
-data class FavouritesEntity (
-    @PrimaryKey
+@Entity(tableName = "favourites_table")
+data class FavouritesEntity(
+    @PrimaryKey(autoGenerate = true)
+    val primaryId:Int,
     val trackName: String?,
     val artistName: String?,
     val trackTimeMillis: Long?,
     val artworkUrl100: String?,
-    @ColumnInfo (name="trackId")
-    val trackId: Long?,
+    @ColumnInfo(name = "trackId", typeAffinity=INTEGER)
+    val trackId: Long,
     val collectionName: String?,
     val releaseDate: String?,
     val primaryGenreName: String?,
