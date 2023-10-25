f7e4a251d1f273ec905479e2b9ef1232be4792cb
NikolayRyabcev <nikolas14@mail.ru>
2023-10-25 23:19:21 +0300

    Версия 22.5.5

--- app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/fragments/NewPlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/fragments/NewPlaylistFragment.kt
@@ -1,20 +1,18 @@
 package com.example.playlistmaker.ui.mediaLibrary.fragments
 
-import android.Manifest.permission.READ_MEDIA_IMAGES
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.ViewGroup
 import android.view.WindowManager
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.example.playlistmaker.Manifest
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.NewPlaylistBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -22,6 +20,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class NewPlaylistFragment : Fragment() {
     private lateinit var newPlaylistBinding: NewPlaylistBinding
     private lateinit var bottomNavigator: BottomNavigationView
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // Пользователь дал разрешение, идем во вью-модель загружать фото
+                loadImage()
+                Log.d("Разрешение на загрузку", "дано")
+
+            } else {
+                // Пользователь отказал, ничего не делаем
+                Log.d("Разрешение на загрузку", "отказано")
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,37 +45,12 @@ class NewPlaylistFragment : Fragment() {
 
         //обработка нажатия на область обложки
         newPlaylistBinding.playlistCover.setOnClickListener {
-
-            //проверяем, есть ли доступ к файлам на устройстве
-            var imagePermission = ContextCompat.checkSelfPermission(
-                requireContext(),
-                android.Manifest.permission.READ_MEDIA_IMAGES
-            )
-
-
-            if (imagePermission == PackageManager.PERMISSION_DENIED) {
+            if (checkImagePermission()) loadImage() else {
                 requestPermissions(
-                    arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
                     READ_MEDIA_IMAGES_REQUEST_CODE
                 )
-            }
-            imagePermission = ContextCompat.checkSelfPermission(
-                requireContext(),
-                android.Manifest.permission.READ_MEDIA_IMAGES
-            )
-            if (imagePermission == PackageManager.PERMISSION_GRANTED) {
-                // создаём событие с результатом и передаём в него PickVisualMedia()
-                val pickMedia =
-                    registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-                        // Callback вызовется, когда пользователь выберет картинку
-                        if (uri != null) {
-                            Log.d("PhotoPicker", "Выбранный URI: $uri")
-                        } else {
-                            Log.d("PhotoPicker", "Ничего не выбрано")
-                        }
-                    }
-// Вызываем метод launch и передаём параметр, чтобы предлагались только картинки
-                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                requestPermissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
             }
         }
 
@@ -73,14 +59,49 @@ class NewPlaylistFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (!newPlaylistBinding.playlistNameEditText.text.isNullOrEmpty()) {
-            newPlaylistBinding.createButton.backgroundTintList =
-                (ContextCompat.getColorStateList(requireContext(), R.color.back1))
-            newPlaylistBinding.createButton.isEnabled=true
-        } else {
-            newPlaylistBinding.createButton.backgroundTintList =
-                (ContextCompat.getColorStateList(requireContext(), R.color.settingsIconGray))
-            newPlaylistBinding.createButton.isEnabled=false
+        //устанавливаем цвет кнопки "Создать"
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                turnOffCreateButton()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                turnOnCreateButton()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) {
+                    turnOffCreateButton()
+                } else {
+                    turnOnCreateButton()
+                }
+            }
         }
+        newPlaylistBinding.playlistNameEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun loadImage() {}
+
+    private fun turnOffCreateButton() {
+        newPlaylistBinding.createButton.backgroundTintList =
+            (ContextCompat.getColorStateList(requireContext(), R.color.settingsIconGray))
+        newPlaylistBinding.createButton.isEnabled = false
+    }
+
+    private fun turnOnCreateButton() {
+        newPlaylistBinding.createButton.backgroundTintList =
+            (ContextCompat.getColorStateList(requireContext(), R.color.back1))
+        newPlaylistBinding.createButton.isEnabled = true
+    }
+
+    private fun checkImagePermission(): Boolean {
+        val permissionProvided = ContextCompat.checkSelfPermission(
+            requireContext(), android.Manifest.permission.READ_MEDIA_IMAGES
+        )
+        return permissionProvided == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        const val READ_MEDIA_IMAGES_REQUEST_CODE = 1
     }
 }
