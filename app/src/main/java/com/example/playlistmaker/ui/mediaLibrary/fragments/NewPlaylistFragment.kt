28319a1f0f68342fcac28963cf69bf2d32355369
NikolayRyabcev <nikolas14@mail.ru>
2023-10-26 22:00:30 +0300

    Версия 22.5.7

--- app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/fragments/NewPlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/fragments/NewPlaylistFragment.kt
@@ -2,8 +2,12 @@ package com.example.playlistmaker.ui.mediaLibrary.fragments
 
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -12,6 +16,7 @@ import android.view.View
 import android.view.View.GONE
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
@@ -20,6 +25,8 @@ import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.NewPlaylistBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tbruyelle.rxpermissions3.RxPermissions
+import java.io.File
+import java.io.FileOutputStream
 
 class NewPlaylistFragment : Fragment() {
     private lateinit var newPlaylistBinding: NewPlaylistBinding
@@ -35,7 +42,7 @@ class NewPlaylistFragment : Fragment() {
         bottomNavigator = requireActivity().findViewById(R.id.bottomNavigationView)
         bottomNavigator.visibility = GONE
 
-      return newPlaylistBinding.root
+        return newPlaylistBinding.root
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -43,6 +50,7 @@ class NewPlaylistFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val rxPermissions = RxPermissions(this)
+
         //устанавливаем цвет кнопки "Создать"
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -63,22 +71,53 @@ class NewPlaylistFragment : Fragment() {
         }
         newPlaylistBinding.playlistNameEditText.addTextChangedListener(simpleTextWatcher)
 
+        //переменеая с лямбдой, которая берет изображение и сохраняет его в ханилище
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    newPlaylistBinding.playlistCover.setImageURI(uri)
+                    saveImageToPrivateStorage(uri)
+                } else {
+                    //ничего не делаем
+                }
+            }
+
         //обработка нажатия на область обложки
         newPlaylistBinding.playlistCover.setOnClickListener {
             rxPermissions.request(android.Manifest.permission.READ_MEDIA_IMAGES)
                 .subscribe { granted: Boolean ->
                     if (granted) {
-                        loadImage()
+                        pickMedia.launch(
+                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                        )
                         Log.d("Разрешение на загрузку", "дано")
                     } else {
                         // Пользователь отказал, ничего не делаем
+                        pickMedia.launch(
+                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                        )
                         Log.d("Разрешение на загрузку", "отказано")
                     }
                 }
         }
     }
 
-    private fun loadImage() {}
+    private fun saveImageToPrivateStorage(uri: Uri) {
+        val filePath =
+            File(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myalbum")
+        if (!filePath.exists()) {
+            filePath.mkdirs()
+        }
+        val file = File(filePath, "first_cover.jpg")
+        val inputStream = requireActivity().contentResolver.openInputStream(uri)
+        //пока не нужно, но пусть полежит
+        val outputStream = FileOutputStream(file)
+        BitmapFactory
+            .decodeStream(inputStream)
+            //.compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
+        Log.d("Разрешение на загрузку", "файл записан")
+    }
+
 
     private fun turnOffCreateButton() {
         newPlaylistBinding.createButton.backgroundTintList =
