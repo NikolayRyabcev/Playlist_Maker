b532790e361cc8f402ae0b9f79c87c7975a4c8c8
NikolayRyabcev <nikolas14@mail.ru>
2023-10-29 11:46:31 +0300

    Версия 22.6.12

--- app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/adapters/PlaylistViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/adapters/PlaylistViewHolder.kt
@@ -1,8 +1,22 @@
 package com.example.playlistmaker.ui.mediaLibrary.adapters
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.PlaylistLayoutBinding
+import com.example.playlistmaker.domain.models.Playlist
 
-class PlaylistViewHolder(view: View) :RecyclerView.ViewHolder(view) {
-    private val name :String
+class PlaylistViewHolder(private val binding: PlaylistLayoutBinding) :RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Playlist) {
+        binding.playlistlittleName.text = item.playlistName
+        binding.playlistlittleSongNumber.text = item.arrayNumber.toString()
+
+        val radius = itemView.resources.getDimensionPixelSize(R.dimen.trackCornerRadius)
+        Glide.with(itemView)
+            .load(item.uri)
+            .placeholder(R.drawable.musicalbum)
+            .transform(RoundedCorners(radius))
+            .into(binding.playlistlittleCover)
+    }
 }
