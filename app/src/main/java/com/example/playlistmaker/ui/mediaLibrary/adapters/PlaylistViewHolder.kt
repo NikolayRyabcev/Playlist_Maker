7940147567f8044aa85e551d0e0f350a7f8d0438
Nikolay <nikolas14@mail.ru>
2023-10-30 17:41:01 +0300

    Версия 22.7.5

--- app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/adapters/PlaylistViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediaLibrary/adapters/PlaylistViewHolder.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmaker.ui.mediaLibrary.adapters
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -7,16 +9,28 @@ import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.PlaylistLayoutBinding
 import com.example.playlistmaker.domain.models.Playlist
 
-class PlaylistViewHolder(private val binding: PlaylistLayoutBinding) :RecyclerView.ViewHolder(binding.root) {
+class PlaylistViewHolder(private val binding: PlaylistLayoutBinding) :
+    RecyclerView.ViewHolder(binding.root) {
     fun bind(item: Playlist) {
         binding.playlistlittleName.text = item.playlistName
         binding.playlistlittleSongNumber.text = item.arrayNumber.toString()
 
         val radius = itemView.resources.getDimensionPixelSize(R.dimen.trackCornerRadius)
-        Glide.with(itemView)
-            .load(item.uri)
-            .placeholder(R.drawable.musicalbum)
-            .transform(RoundedCorners(radius))
-            .into(binding.playlistlittleCover)
+
+        if (item.uri.isNullOrEmpty()) {
+            val imageResource = R.drawable.musicalbum
+            binding.playlistlittleCover.setImageResource(imageResource)
+            val layoutParams = binding.playlistlittleCover.layoutParams
+            layoutParams.width = 104
+            layoutParams.height = 103
+            binding.playlistlittleCover.layoutParams = layoutParams
+        } else {
+
+            Glide.with(itemView)
+                .load(item.uri)
+                .placeholder(R.drawable.musicalbum)
+                .transform(RoundedCorners(radius))
+                .into(binding.playlistlittleCover)
+        }
     }
 }
