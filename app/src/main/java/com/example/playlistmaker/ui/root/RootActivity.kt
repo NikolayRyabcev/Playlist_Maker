44fb154fba0c8232461eb15eaae458d514fc4a77
NikolayRyabcev <nikolas14@mail.ru>
2024-01-06 16:18:25 +0300

    Сделали компоуз

--- app/src/main/java/com/example/playlistmaker/ui/root/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/root/RootActivity.kt
@@ -1,31 +1,34 @@
 package com.example.playlistmaker.ui.root
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.fragment.app.commit
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.example.playlistmaker.R
-import com.example.playlistmaker.databinding.ActivityRootBinding
-import com.example.playlistmaker.ui.mediaLibrary.fragments.MediaLibraryFragment
-import com.example.playlistmaker.ui.search.fragments.SearchFragment
-import com.example.playlistmaker.ui.settings.fragments.SettingsFragment
-import com.example.playlistmaker.ui.theme.PlaylistTheme
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class RootActivity : AppCompatActivity() {
+class RootActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PlaylistTheme {
-                Surface {
-                    Text("Hello world")
-                }
-            }
+            Text("Hello world!")
         }
-
     }
+}
+
+@Composable
+fun MyComposable() {
+    MaterialTheme {
+        Surface {
+            Text(text = "Hello, Compose!")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMyComposable() {
+    MyComposable()
 }
