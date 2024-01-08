25d17cc776d30f93e6cc19ef814b5b00d3f526e2
NikolayRyabcev <nikolas14@mail.ru>
2024-01-08 08:34:08 +0300

    Первая вью

--- app/src/main/java/com/example/playlistmaker/ui/settings/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/settings/fragments/SettingsFragment.kt
@@ -5,21 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.playlistmaker.R
@@ -82,23 +87,53 @@ class SettingsFragment : Fragment() {
                     start = dimensionResource(id = R.dimen.left1)
                 )
         ) {
-            SettingsText()
+            settingsText()
+            darkTheme()
         }
     }
 
     @Composable
-    fun SettingsText() {
+    fun settingsText() {
         val context = LocalContext.current
         val textColor = ContextCompat.getColor(context, R.color.black)
+        val customFont = FontFamily(Font(resId = R.font.ys_display_medium))
         Text(
             text = stringResource(id = R.string.SettingsString),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(
                     top = dimensionResource(id = R.dimen.settingsTopString14dp),
-                    bottom = 0.dp
+                    bottom = 0.dp,
                 ),
             color = Color(textColor),
+            fontFamily = customFont,
+            fontSize = 22.sp
+        )
+    }
+
+    @Composable
+    fun darkTheme() {
+        val context = LocalContext.current
+        val textColor = ContextCompat.getColor(context, R.color.black)
+        val customFont = FontFamily(Font(resId = R.font.ys_display_regular))
+        Box(Modifier.padding(top = 40.dp)) {
+            Text(
+                text = stringResource(id = R.string.BlackTheme),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = dimensionResource(id = R.dimen.settingsTopString14dp),
+                        bottom = 0.dp,
+                    ),
+                color = Color(textColor),
+                fontFamily = customFont,
+                fontSize = 22.sp
             )
+            Switch(
+                checked = false,
+                onCheckedChange = {},
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
     }
 }
