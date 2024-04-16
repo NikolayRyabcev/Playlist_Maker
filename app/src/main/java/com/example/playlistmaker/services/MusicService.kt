2ecdaf2e6ea39cbc13d8449b71d9be7342981bcd
NikolayRyabcev <nikolas14@mail.ru>
2024-04-17 01:12:28 +0300

    переделать стейты и закинуть обработку во фрагмент

--- app/src/main/java/com/example/playlistmaker/services/MusicService.kt
+++ app/src/main/java/com/example/playlistmaker/services/MusicService.kt
@@ -1,18 +1,101 @@
-package com.example.playlistmaker.services
-
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Binder
 import android.os.IBinder
+import com.example.playlistmaker.domain.player.PlayerState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MusicService : Service() {
-    private val binder=MusicServiceBinder()
+
+    private val binder = MusicServiceBinder()
+
+    private val _playerState = MutableStateFlow<PlayerState>(PlayerState.Default())
+    val playerState = _playerState.asStateFlow()
+
+    private var songUrl = ""
+
+    private var mediaPlayer: MediaPlayer? = null
+
+    private var timerJob: Job? = null
+
+    private fun startTimer() {
+        timerJob = CoroutineScope(Dispatchers.Default).launch {
+            while (mediaPlayer?.isPlaying == true) {
+                delay(200L)
+                _playerState.value = PlayerState.Playing(getCurrentPlayerPosition())
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer()
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
+        songUrl = intent?.getStringExtra("song_url") ?: ""
+        initMediaPlayer()
         return binder
     }
 
+    override fun onUnbind(intent: Intent?): Boolean {
+        releasePlayer()
+        return super.onUnbind(intent)
+    }
+
+    // Методы управления Media Player
+
+    private fun initMediaPlayer() {
+        if (songUrl.isEmpty()) return
+
+        mediaPlayer?.setDataSource(songUrl)
+        mediaPlayer?.prepareAsync()
+        mediaPlayer?.setOnPreparedListener {
+            _playerState.value = PlayerState.Prepared()
+        }
+        mediaPlayer?.setOnCompletionListener {
+            _playerState.value = PlayerState.Prepared()
+        }
+    }
+
+    fun startPlayer() {
+        mediaPlayer?.start()
+        _playerState.value = PlayerState.Playing(getCurrentPlayerPosition())
+        startTimer()
+    }
+
+    fun pausePlayer() {
+        mediaPlayer?.pause()
+        timerJob?.cancel()
+        _playerState.value = PlayerState.Paused(getCurrentPlayerPosition())
+    }
+
+    private fun releasePlayer() {
+        timerJob?.cancel()
+        mediaPlayer?.stop()
+        _playerState.value = PlayerState.Default()
+        mediaPlayer?.setOnPreparedListener(null)
+        mediaPlayer?.setOnCompletionListener(null)
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    private fun getCurrentPlayerPosition(): String {
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer?.currentPosition) ?: "00:00"
+    }
+
+    // Binder
+
     inner class MusicServiceBinder : Binder() {
-        fun getMusicService(): MusicService = this@MusicService
+        fun getService(): MusicService = this@MusicService
     }
 }
