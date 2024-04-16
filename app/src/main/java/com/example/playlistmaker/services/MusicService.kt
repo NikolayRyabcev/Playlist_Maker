12e63f72b57641486bcdd028b7864826c399a997
NikolayRyabcev <nikolas14@mail.ru>
2024-04-16 18:47:21 +0300

    Создаем сервис

---
+++ app/src/main/java/com/example/playlistmaker/services/MusicService.kt
@@ -0,0 +1,18 @@
+package com.example.playlistmaker.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+
+class MusicService : Service() {
+    private val binder=MusicServiceBinder()
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return binder
+    }
+
+    inner class MusicServiceBinder : Binder() {
+        fun getMusicService(): MusicService = this@MusicService
+    }
+}
