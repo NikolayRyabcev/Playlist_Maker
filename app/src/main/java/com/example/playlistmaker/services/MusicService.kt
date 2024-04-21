ffccca2da9d7d3ba0ca4bb7979b8a8b0b5a1f823
Nikolay <nikolas14@mail.ru>
2024-04-21 21:36:25 +0300

    готовим показ уведомлений

--- app/src/main/java/com/example/playlistmaker/services/MusicService.kt
+++ app/src/main/java/com/example/playlistmaker/services/MusicService.kt
@@ -6,12 +6,14 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 import com.example.playlistmaker.R
 import com.example.playlistmaker.domain.player.PlayerState
 import kotlinx.coroutines.CoroutineScope
@@ -51,6 +53,20 @@ class MusicService : Service(), AudioPlayerControl {
         super.onCreate()
         mediaPlayer = MediaPlayer()
         createNotificationChannel()
+        ServiceCompat.startForeground(
+           this,
+            SERVICE_NOTIFICATION_ID,
+            createServiceNotification(),
+            getForegroundServiceTypeConstant()
+        )
+    }
+
+    private fun getForegroundServiceTypeConstant(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
+        } else {
+            0
+        }
     }
 
     private fun createNotificationChannel() {
@@ -65,7 +81,8 @@ class MusicService : Service(), AudioPlayerControl {
         )
         channel.description = "Service for playing music"
 
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
     }
 
@@ -130,7 +147,7 @@ class MusicService : Service(), AudioPlayerControl {
         mediaPlayer = null
     }
 
-    fun getCurrentPlayerPosition(): String {
+    private fun getCurrentPlayerPosition(): String {
         return SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer?.currentPosition)
             ?: "00:00"
     }
@@ -142,5 +159,6 @@ class MusicService : Service(), AudioPlayerControl {
     private companion object {
         const val LOG_TAG = "MusicService"
         const val NOTIFICATION_CHANNEL_ID = "music_service_channel"
+        const val SERVICE_NOTIFICATION_ID = 100
     }
 }
